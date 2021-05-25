c7be52f99a55c930da78a52baf6a12a79af412c0
jaden <cykreet@gmail.com>
2021-05-25 17:30:15 +0200

    fix: redis cache tests

--- test/RedisCache.test.ts
+++ test/RedisCache.test.ts
@@ -7,7 +7,7 @@ const client = new Redis();
 
 describe("Kas Redis Cache", () => {
   test("Should cache data for 2 seconds", async () => {
-    const cache = new RedisMapCache<string>(client, { defaultExpiry: "2s" });
+    const cache = new RedisMapCache<string>(client, "fortnite", { defaultExpiry: "2s" });
     expect(await cache.set("test", "sweet")).toBeTruthy();
     expect(await cache.get("test")).toBe("sweet");
 
@@ -15,21 +15,21 @@ describe("Kas Redis Cache", () => {
     expect(await cache.get("test")).toBeUndefined();
   });
   test("Should delete cached data", async () => {
-    const cache = new RedisMapCache<string>(client);
+    const cache = new RedisMapCache<string>(client, "fortnite");
     expect(await cache.set("test", "sweet")).toBeTruthy();
     expect(await cache.get("test")).toBe("sweet");
     expect(await cache.delete("test")).toBeTruthy();
     expect(await cache.get("test")).toBeUndefined();
   });
   test("Should clear all cached data", async () => {
-    const cache = new RedisMapCache<string>(client);
+    const cache = new RedisMapCache<string>(client, "fortnite");
     expect(await cache.set("test", "sweet", undefined, SetOption.KEEP_TTL)).toBeTruthy();
     expect(await cache.get("test")).toBe("sweet");
     expect(await cache.clear()).toBeUndefined();
     expect(await cache.get("test")).toBeUndefined();
   });
   test("Should check if cached data exists", async () => {
-    const cache = new RedisMapCache<string>(client);
+    const cache = new RedisMapCache<string>(client, "fortnite");
     expect(await cache.set("test", "sweet")).toBeTruthy();
     expect(await cache.has("test")).toBeTruthy();
     expect(await cache.delete("test")).toBeTruthy();
