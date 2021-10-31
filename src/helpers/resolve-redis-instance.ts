ec397659edd35d550c6439b554595c3402051261
cykreet <cykreet@gmail.com>
2021-10-31 18:10:29 +0000

    fix: remove non-ioredis options before resolving instance

--- src/helpers/resolve-redis-instance.ts
+++ src/helpers/resolve-redis-instance.ts
@@ -5,11 +5,11 @@ export function resolveRedisInstance(host: RedisHost, options?: Redis.RedisOptio
   if (host instanceof Redis || typeof Redis) return host as Redis.Redis;
   // host parameter being used as connection uri
   if (typeof host === "string") return new Redis(host, options);
-  if (isRedisOptions(host)) return new Redis(host);
+  if (isRedisHostOptions(host)) return new Redis(host);
   throw new Error("Could not resolve redis connection instance.");
 }
 
-function isRedisOptions(input: unknown): input is Redis.RedisOptions {
+function isRedisHostOptions(input: unknown): input is Redis.RedisOptions {
   if (typeof input !== "object") throw new Error("Expected input to be an object.");
   if (input === null) throw new Error("Input cannot be null.");
   return "host" in input && "port" in input;
