b06ead3427f70034c87bb2122d3b5b5f8b5179ea
cykreet <cykreet@gmail.com>
2021-12-09 18:25:42 +0000

    i have no idea if this even did anything

--- src/helpers/resolve-redis-instance.ts
+++ src/helpers/resolve-redis-instance.ts
@@ -2,7 +2,7 @@ import Redis from "ioredis";
 
 export type RedisLike = string | Redis.RedisOptions | Redis.Redis;
 export function resolveRedisInstance(host: RedisLike, options?: Redis.RedisOptions): Redis.Redis {
-  if (host instanceof Redis || typeof Redis) return host as Redis.Redis;
+  if (host instanceof Redis) return host as Redis.Redis;
   // host parameter being used as connection uri
   if (typeof host === "string") return new Redis(host, options);
   if (isRedisHostOptions(host)) return new Redis(host);
